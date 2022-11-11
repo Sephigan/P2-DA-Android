97fc01dfb2ad8422aa1252ffe6b1558d468036b5
Sephigan <117346105+Sephigan@users.noreply.github.com>
2022-11-11 10:17:53 +0100

    UserRepository_modifié

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.openclassrooms.magicgithub.repository;
 
 import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS;
+import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS_RANDOM;
 
 import com.openclassrooms.magicgithub.api.ApiService;
 import com.openclassrooms.magicgithub.model.User;
@@ -21,10 +22,21 @@ public class UserRepository {
     }
 
     public void generateRandomUser() {
-        // TODO: A modifier
+        // TODO: A tester
+        String a = new String();
+        String b = new String();
+        String c = new String();
+        User add_User = new User(a, b, c);
+        for(int x=FAKE_USERS.size(); x<25; x++)
+        {
+            for(int y=0; y<=4; y++) {
+                FAKE_USERS.add(x, add_User = FAKE_USERS_RANDOM.get(y));
+            }
+        }
     }
 
     public void deleteUser(User user) {
-        // TODO: A modifier
+        // TODO: A tester
+        FAKE_USERS.remove(user.getId());
     }
 }
